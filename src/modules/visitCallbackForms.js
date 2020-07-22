74be7b41aea4e8b64a8c30273ed6ea0993b3e5b6
Anna Ponyavina <ponyavina1997@mail.ru>
2020-07-22 20:01:59 +0300

    create (popupGift)

--- src/modules/visitCallbackForms.js
+++ src/modules/visitCallbackForms.js
@@ -2,10 +2,12 @@ const visitCallbackForms = () => {
 
     const popupVisitForm = document.getElementById('free_visit_form'),
         popupCallbackForm = document.getElementById('callback_form'),
+        fixedGift = document.querySelector('.fixed-gift'),
+        popupGift = document.getElementById('gift'),
         popupClose = (item) => {
             item.addEventListener('click', (e) => {
                 let target = e.target;
-                if (target.classList.contains('close-icon')) {
+                if (target.classList.contains('close-icon') || target.classList.contains('close-btn')) {
                     item.style.display = 'none';
                 } else {
                     target = target.closest('.form-content');
@@ -25,6 +27,11 @@ const visitCallbackForms = () => {
         if (target.classList.contains('callback-btn')) {
             popupCallbackForm.style.display = 'flex';
             popupClose(popupCallbackForm);
+        }
+        if (target.classList.contains('fixed-gift-img')) {
+            popupGift.style.display = 'flex';
+            fixedGift.style.display = 'none';
+            popupClose(popupGift);
         }
     });
 };
