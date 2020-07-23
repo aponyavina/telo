6d7051ecd1793b727c5c6d11dceba3c9efef156f
Anna Ponyavina <ponyavina1997@mail.ru>
2020-07-23 11:32:57 +0300

    fix(clubSelect)

--- src/modules/clubSelect.js
+++ src/modules/clubSelect.js
@@ -4,7 +4,9 @@ const clubSelect = () => {
         const clubsListItems = document.querySelector('.clubs-list-items');
         if (target.classList.contains('clubs-list-title')) {
             clubsListItems.style.display = 'block';
-        } else if (!target.classList.contains('clubs-list-item')) {
+        } else
+        if (!target.classList.contains('clubs-list-items') && 
+        !target.classList.contains('clubs-list-item')) {
             clubsListItems.style.display = 'none';
         }
     });
