16a93e080305f26db9a3195195a477285a50867a
Anna Ponyavina <ponyavina1997@mail.ru>
2020-07-23 14:42:10 +0300

    fix(scrolling)

--- src/modules/scrolling.js
+++ src/modules/scrolling.js
@@ -3,9 +3,12 @@ const scrolling = () => {
 
     const menuButton = document.querySelector('.menu-button'),
         topMenu = document.querySelector('.top-menu'),
+        toTop = document.getElementById('totop'),
         clubsBlock = document.getElementById('clubs');
 
-    document.body.addEventListener('scroll', () => {
+    toTop.style.display = 'none';
+
+    document.addEventListener('scroll', () => {
         if (window.innerWidth < 768) {
             menuButton.style.display = 'block';
             const top = window.pageYOffset;
@@ -16,10 +19,13 @@ const scrolling = () => {
                 topMenu.style.position = 'static';
             }
         }
-        console.log(clubsBlock.getBoundingClientRect().top);
+
+        if (clubsBlock.getBoundingClientRect().top <= 0) {
+            toTop.style.display = 'block';
+        } else {
+            toTop.style.display = 'none';
+        }
     });
-
-
 };
 
 export default scrolling;
