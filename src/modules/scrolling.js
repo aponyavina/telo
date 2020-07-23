b573e999c76d879f7fb948ec3a00402035a6af21
Anna Ponyavina <ponyavina1997@mail.ru>
2020-07-23 14:27:45 +0300

    fix scrolling

--- src/modules/scrolling.js
+++ src/modules/scrolling.js
@@ -3,25 +3,23 @@ const scrolling = () => {
 
     const menuButton = document.querySelector('.menu-button'),
         topMenu = document.querySelector('.top-menu'),
-        headerMain = document.querySelector('.header-main');
-        console.log(headerMain);
+        clubsBlock = document.getElementById('clubs');
 
+    document.body.addEventListener('scroll', () => {
         if (window.innerWidth < 768) {
             menuButton.style.display = 'block';
-            document.addEventListener('scroll', () => {
-                const top = window.pageYOffset;
-                console.log(headerMain.getBoundingClientRect().top);
-                if (topMenu.getBoundingClientRect().top <= 0) {
-                    topMenu.style.position = 'fixed';
-                }
-                if (top < 187) {
-                    topMenu.style.position = 'static';
-                }
-                console.log('ff');
-            });
-        } 
+            const top = window.pageYOffset;
+            if (topMenu.getBoundingClientRect().top <= 0) {
+                topMenu.style.position = 'fixed';
+            }
+            if (top < 187) {
+                topMenu.style.position = 'static';
+            }
+        }
+        console.log(clubsBlock.getBoundingClientRect().top);
+    });
 
-       
+
 };
 
 export default scrolling;
